402a3c53ccd72fa7f86dd611a2e20a3adcb2c1e5
beron <bleink@outlook.es>
2021-02-11 16:59:15 +0100

    App.js - add Nav in aside element, along with article inside section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {Nav} from './assets/nav'
 import {Enuncement} from './assets/enuncement'
 import {Exercise01} from './exercise01/exercise01'
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1>CIPSA - CSS Exercises</h1>
-      </header>
-      <Enuncement/>
-      <Exercise01/>
-    </div>
+    <>
+    <header>
+      <h1>CIPSA - CSS Exercises</h1>
+    </header>
+    <section class="flex-row">
+      <aside>
+        <Nav/>
+      </aside>
+      <article>
+        <Enuncement/>
+        <Exercise01/>
+      </article>
+    </section>
+    </>
   );
 }
 
