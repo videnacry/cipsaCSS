8b2f53259b97153063158d58c91f4e32308ee1bd
beron <bleink@outlook.es>
2021-02-11 15:58:08 +0100

    App.js - add exercice01

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Exercise01} from './exercise01/exercise01'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Exercise01/>
     </div>
   );
 }
