3d1926e0a898e5c3dde9149d9b8371463927103b
beron <bleink@outlook.es>
2021-02-11 16:24:44 +0100

    App.js - replace React default content per h1

--- src/App.js
+++ src/App.js
@@ -6,19 +6,8 @@ import {Exercise01} from './exercise01/exercise01'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>CIPSA - CSS Exercises</h1>
       </header>
       <Enuncement/>
       <Exercise01/>
