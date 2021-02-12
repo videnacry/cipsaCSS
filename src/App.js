a636a3983e54188ac95bde883eafb3fd11ede0a2
beron <bleink@outlook.es>
2021-02-12 12:18:28 +0100

    App.js - add reducer example to change exercise to be shown

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import {useReducer} from 'react'
 import './App.css';
 import {Nav} from './assets/nav'
 import {Enuncement} from './assets/enuncement'
 import {Exercise01} from './exercise01/exercise01'
 
+const initialState = {exerciseIndex : 0}
+const reducer = (state, action) => {
+  switch (action.exercise) {
+    case '1': return {...state, exerciseIndex : 1}
+    case '2': return {...state, exerciseIndex : 2}
+  }
+}
+const exercises = [<Exercise01/>]
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  console.log(state)
   return (
     <>
     <header>
-      <h1>CIPSA - CSS Exercises</h1>
+      <h1 onClick={() => dispatch({exercise : '2'})}>CIPSA - CSS Exercises</h1>
     </header>
-    <section class="flex-row">
+    <section className="flex-row">
       <aside>
         <Nav/>
       </aside>
       <article>
         <Enuncement/>
-        <Exercise01/>
+        {exercises[state.exerciseIndex] ? exercises[state.exerciseIndex] : console.log(state.exerciseIndex)}
       </article>
     </section>
     </>
