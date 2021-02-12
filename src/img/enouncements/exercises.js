2fbd78f9a69bd234b3756605d7f8df2ef690b0ea
beron <bleink@outlook.es>
2021-02-12 20:07:22 +0100

    exercises.js - export exercise[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].jpg

---
+++ src/img/enouncements/exercises.js
@@ -0,0 +1,24 @@
+
+import exercise1 from './exercise1.jpg'
+import exercise2 from './exercise2.jpg'
+import exercise3 from './exercise3.jpg'
+import exercise4 from './exercise4.jpg'
+import exercise5 from './exercise5.jpg'
+import exercise6 from './exercise6.jpg'
+import exercise7 from './exercise7.jpg'
+import exercise8 from './exercise8.jpg'
+import exercise9 from './exercise9.jpg'
+import exercise10 from './exercise10.jpg'
+
+export const enouncementsImg = [
+    exercise1, 
+    exercise2, 
+    exercise3, 
+    exercise4, 
+    exercise5, 
+    exercise6, 
+    exercise7, 
+    exercise8, 
+    exercise9, 
+    exercise10
+]
