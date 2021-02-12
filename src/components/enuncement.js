27be0564b61f41eeb162085c2f8a4f341a0dea9c
beron <bleink@outlook.es>
2021-02-12 18:36:08 +0100

    enuncement.js - make title and text pend on props

--- src/components/enuncement.js
+++ src/components/enuncement.js
@@ -1,10 +1,10 @@
 import './enuncement.css'
 
-export const Enuncement = () => {
+export const Enuncement = ({enuncement:{title, text}}) => {
     return(
         <>
-        <h1 className="enuncement-title">Ejercicio 1</h1>
-        <p className="enuncement-text">A partir del c&oacute;digo HTML y CSS que se muestra, a&ntilde;adir los selectores CSS que faltan para aplicar los estilos deseados. Cada regla CSS incluye un comentaroi en el que se explica los elementos a los que debe aplicarse</p>
+        <h1 className="enuncement-title">{title}</h1>
+        <p className="enuncement-text">{text}</p>
         </>
     )
 }
