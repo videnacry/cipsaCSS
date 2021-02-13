faaff65584dfa0e0eac199f44932552adb90c341
beron <bleink@outlook.es>
2021-02-13 16:34:18 +0100

    exercise02.[js, css]

---
+++ src/exercise02/exercise02.js
@@ -0,0 +1,20 @@
+import './exercise02.css'
+const links = [
+    'https://www.cipsa.net/',
+    'www.cipsaempresas.net',
+    'www.maps.google.com',
+    'https://www.google.es/maps/place/CIPSA+-+Formaci%C3%B3+A+Barcelona',
+    'https://www.google.es/'
+]
+
+export const Exercise02 = () => {
+    return(
+        <ul>
+            {links.map(link => 
+                <li className='m-1 p-1 bg-blue hover-color-purple'>
+                    <a href={link} className='color-white visited-color-red'>{link}</a>
+                </li>)
+            }
+        </ul>
+    )
+}
