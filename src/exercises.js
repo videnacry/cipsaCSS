8973568c5da200915aec01e08b19a079a16e4952
beron <bleink@outlook.es>
2021-02-13 16:35:08 +0100

    exercises.js - add export exercise02

--- src/exercises.js
+++ src/exercises.js
@@ -1,3 +1,7 @@
 import {Exercise01} from './exercise01/exercise01'
+import {Exercise02} from './exercise02/exercise02'
 
-export const exercises = [<Exercise01/>]
+export const exercises = [
+<Exercise01/>,
+<Exercise02/>
+]
