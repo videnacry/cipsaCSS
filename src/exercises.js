3461045358552416861b110194e33688a148e859
beron <bleink@outlook.es>
2021-02-15 22:47:36 +0100

    exercises.js - add exercise05

--- src/exercises.js
+++ src/exercises.js
@@ -2,10 +2,12 @@ import {Exercise01} from './exercise01/exercise01'
 import {Exercise02} from './exercise02/exercise02'
 import {Exercise03} from './exercise03/exercise03'
 import {Exercise04} from './exercise04/exercise04'
+import {Exercise05} from './exercise05/exercise05'
 
 export const exercises = [
 <Exercise01/>,
 <Exercise02/>,
 <Exercise03/>,
-<Exercise04/>
+<Exercise04/>,
+<Exercise05/>
 ]
