e1a2a1cbede9d47477ff1468357abe1bd4a160ba
beron <bleink@outlook.es>
2021-02-15 17:17:22 +0100

    exercise04

--- src/exercises.js
+++ src/exercises.js
@@ -1,9 +1,11 @@
 import {Exercise01} from './exercise01/exercise01'
 import {Exercise02} from './exercise02/exercise02'
 import {Exercise03} from './exercise03/exercise03'
+import {Exercise04} from './exercise04/exercise04'
 
 export const exercises = [
 <Exercise01/>,
 <Exercise02/>,
-<Exercise03/>
+<Exercise03/>,
+<Exercise04/>
 ]
