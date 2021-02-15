8201fb78f1371886bf2b224dbe9f465ac08c1084
beron <bleink@outlook.es>
2021-02-15 20:51:30 +0100

    exercise04.js - add tbody

--- src/exercise04/exercise04.js
+++ src/exercise04/exercise04.js
@@ -5,16 +5,18 @@ export const Exercise04 = () => {
     return(
         <table className="table-bordered border-2">
             <caption className='caption-side-bottom'>TABLA 1</caption>
-            <tr>
-                <td className='border-2'>c-1 f-1</td>
-                <td>c-2 f-1</td>
-                <td>c-3 f-1</td>
-            </tr>
-            <tr>
-                <td>c-1 f-2</td>
-                <td>c-2 f-2</td>
-                <td>c-3 f-2</td>
-            </tr>
+            <tbody>
+                <tr>
+                    <td className='border-2'>c-1 f-1</td>
+                    <td>c-2 f-1</td>
+                    <td>c-3 f-1</td>
+                </tr>
+                <tr>
+                    <td>c-1 f-2</td>
+                    <td>c-2 f-2</td>
+                    <td>c-3 f-2</td>
+                </tr>
+            </tbody>
         </table>
     )
 }
