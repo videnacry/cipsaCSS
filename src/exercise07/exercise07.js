5c1247493e9cb5ae610dcd5ccfc230ef6dbc6a80
beron <bleink@outlook.es>
2021-02-19 23:41:19 +0100

    exercise07[.js, .css] - make exercise07 styles only affect children of elements with .exercise07 class

--- src/exercise07/exercise07.js
+++ src/exercise07/exercise07.js
@@ -7,42 +7,44 @@ const boldRedClass = 'font-weight-900 color-red'
 export const Exercise07 = () => {
 
     return(
-    <div className='flex-row justify-content-center w-max'>
-        <div className='w-800 h-700 border-black border-4'>
-            <div className='m-2'>
-                <header className='h-100 border-black mb-1 position-relative'>
-                    <h1 className={'absolute-center m-0 ' + boldRedClass}>
-                        Videnacry
-                    </h1>
-                </header>
-                <nav className='h-50 border-black mb-1'>
-                    <ul className='flex-row justify-content-center list-style-none'>
-                        <li className='mx-2'>
-                            <b className={boldGreenClass}>Inicio</b>
-                        </li>
-                        <li className='mx-2'>
-                            <b className={boldGreenClass}>P&aacute;ginas</b>
-                        </li>
-                        <li>
-                            <b className={boldGreenClass}>Contacto</b>
-                        </li>
-                    </ul>
-                </nav>
-                <article className='border-black mb-1'>
-                    <p>
-                        <b className={boldRedClass}>
-                            Hace mucho que existe una fusi&oacute;n entre la causa y la consecuencia, siendo que se habla de ellas en algunas situaciones, como si fuesen lo mismo.
-                        </b>
-                    </p>
-                    <div className='flex-row justify-content-center'>
-                        <img src={goat} width='200' alt='semblante de cabra con un lobo, rosas, un feto y una persona en el'/>
-                    </div>
-                    <p>
-                        <b className={boldRedClass}>
-                            Otra fusi&oacute;n divertida es la de una propiedad con una categorizaci&oacute; con el mismo objeto, lo que termina haciendo que el objeto se reduzca a una simple perspectiva bajo un especifico escenario.
-                        </b>
-                    </p>
-                </article>
+    <div className='exercise07'>
+        <div className='flex-row justify-content-center w-max'>
+            <div className='w-800 h-700 border-black border-4'>
+                <div className='m-2'>
+                    <header className='h-100 border-black mb-1 position-relative'>
+                        <h1 className={'absolute-center m-0 ' + boldRedClass}>
+                            Videnacry
+                        </h1>
+                    </header>
+                    <nav className='h-50 border-black mb-1'>
+                        <ul className='flex-row justify-content-center list-style-none'>
+                            <li className='mx-2'>
+                                <b className={boldGreenClass}>Inicio</b>
+                            </li>
+                            <li className='mx-2'>
+                                <b className={boldGreenClass}>P&aacute;ginas</b>
+                            </li>
+                            <li>
+                                <b className={boldGreenClass}>Contacto</b>
+                            </li>
+                        </ul>
+                    </nav>
+                    <article className='border-black mb-1'>
+                        <p>
+                            <b className={boldRedClass}>
+                                Hace mucho que existe una fusi&oacute;n entre la causa y la consecuencia, siendo que se habla de ellas en algunas situaciones, como si fuesen lo mismo.
+                            </b>
+                        </p>
+                        <div className='flex-row justify-content-center'>
+                            <img src={goat} width='200' alt='semblante de cabra con un lobo, rosas, un feto y una persona en el'/>
+                        </div>
+                        <p>
+                            <b className={boldRedClass}>
+                                Otra fusi&oacute;n divertida es la de una propiedad con una categorizaci&oacute; con el mismo objeto, lo que termina haciendo que el objeto se reduzca a una simple perspectiva bajo un especifico escenario.
+                            </b>
+                        </p>
+                    </article>
+                </div>
             </div>
         </div>
     </div>
