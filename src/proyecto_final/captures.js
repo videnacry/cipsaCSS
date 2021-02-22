04e319ca575b819edfb0d8d37d91ac86e3a64032
beron <bleink@outlook.es>
2021-02-22 10:45:58 +0100

    captures.js, [contacto, index, proyectos, servicios, final-project].jpg - export images

---
+++ src/proyecto_final/captures.js
@@ -0,0 +1,7 @@
+import enouncementImg from '../img/enouncements/final-project.jpg'
+import indexImg from '../img/captions/index.png'
+import projectsImg from '../img/captions/proyectos.png'
+import servicesImg from '../img/captions/servicios.png'
+import contactImg from '../img/captions/contacto.png'
+
+export const captures = [enouncementImg, indexImg, projectsImg, servicesImg, contactImg]
