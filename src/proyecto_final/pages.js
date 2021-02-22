67f639144575b8bd66eec357e8996f2b9c6bc602
beron <bleink@outlook.es>
2021-02-22 13:08:12 +0100

    [contact, index, projects, services].js & pages.js - pages creation with a div return and export

--- src/proyecto_final/pages.js
+++ src/proyecto_final/pages.js
@@ -1,2 +1,11 @@
-
-export const pages = [<div></div>]
+import {Index} from './index'
+import {Projects} from './projects'
+import {Services} from './services'
+import {Contact} from './contact'
+export const pages = [
+    <div></div>,
+    <Index/>,
+    <Projects/>,
+    <Services/>,
+    <Contact/>
+]
