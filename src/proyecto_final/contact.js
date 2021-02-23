ebedf2299663f08ba0b14ff3efc6d3331af18674
beron <bleink@outlook.es>
2021-02-23 16:50:14 +0100

    contact.js - htlm structure

--- src/proyecto_final/contact.js
+++ src/proyecto_final/contact.js
@@ -1,7 +1,19 @@
+import {Header} from './header'
 export const Contact = () => {
     return(
-        <div>
-            Contacto
-        </div>
+    <>
+    <Header/>
+    <form>
+        <h3>Contacto</h3>
+        <p>P&oacute;ngase en contacto con nosotros mediante el siguiente formulario.</p>
+        <input type='text' id='name' name='name' placeholder='Nombre' />
+        <input type='email' id='email' name='email' placeholder='Email' />
+        <input type='text' id='subject' name='subject' placeholder='Subject' />
+        <textarea  style={{resize:'none'}}>
+
+        </textarea>
+        <input type='submit' value='Enviar >' />
+    </form>
+    </>
     )
 }
