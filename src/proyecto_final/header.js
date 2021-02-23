a751fc48cc4cf9284e586fc6207b1bac5a051435
beron <bleink@outlook.es>
2021-02-23 16:49:33 +0100

    header.[js, css] - all styles

--- src/proyecto_final/header.js
+++ src/proyecto_final/header.js
@@ -4,17 +4,21 @@ import subtitle from './material/subtitle.png'
 export const Header = () => {
     return(
     <>
-    <header>
-        <img src={logo} alt='' width='30%'/>
-        <img src={subtitle} alt='' width='60%'/>
+    <header className='border-box h-24dot6-vw pt-01-vw flex-column justify-content-center bg-img-gradient-e2e2e2-white-25'>
+        <div className='border-box w-100-percent pr-05-vw flex-row justify-content-center'>
+            <img src={logo} alt='' width='34%'/>
+        </div>
+        <div className='w-100-percent flex-row justify-content-center'>
+            <img src={subtitle} alt='' width='60%'/>
+        </div>
     </header>
-    <nav>
-        <ul className='flex-row list-style-none'>
-            <li>INICIO</li><span/>~<span/>
-            <li>CIPSA</li><span/>~<span/>
-            <li>SERVICIOS</li><span/>~<span/>
-            <li>PROYECTOS</li><span/>~<span/>
-            <li>CONTACTO</li>
+    <nav className='w-100-percent border-00dot25-vw border-l-00 border-r-00 border-solid border-color-e5e5e5 flex-row justify-content-center bg-color-f3f3f4 font-size-1dot8-vw font-weight-500 color-9c89a2'>
+        <ul className='w-65-percent w-max-500 p-00 flex-row justify-content-space-evenly list-style-none'>
+            <li className='color-blueviolet'>INICIO</li><span/>~<span/>
+            <li className='color-blueviolet'>CIPSA</li><span/>~<span/>
+            <li className='color-blueviolet'>SERVICIOS</li><span/>~<span/>
+            <li className='color-blueviolet'>PROYECTOS</li><span/>~<span/>
+            <li className='color-blueviolet'>CONTACTO</li>
         </ul>
     </nav>
     </>
