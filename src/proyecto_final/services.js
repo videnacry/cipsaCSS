7900ad09d9fab3d837c2213dc963fe382bbe882e
beron <bleink@outlook.es>
2021-02-25 17:20:21 +0100

    services.[js, css] - final project services page styles

--- src/proyecto_final/services.js
+++ src/proyecto_final/services.js
@@ -4,49 +4,51 @@ export const Services = () => {
     return(
     <>
     <Header />
-    <div>
-        <h1>Servicios</h1>
-        <article>
-            <h2>
+    <div className='border-box w-56-vw pl-5dot2-vw font-weight-400 font-size-1dot6-vw color-4d5155 text-shadow-minus-0dot1-vw-fcdfaa-0dot1-vw-b5c2d9-0-0-0dot1-vw-6e5571'>
+        <h1 className='mt-4dot4-vw mb-3dot1-vw font-weight-500 font-size-3dot1-vw'>
+            Servicios
+        </h1>
+        <article className='h-18dot1-vw'>
+            <h2 className='m-00 font-weight-400 font-size-2dot4-vw'>
                 User Experience
             </h2>
-            <p>
+            <p className='line-height-2dot3-vw mt-1dot1-vw mb-1dot3-vw'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a nibg mauris. Mauris interdum, dolor in vulputate tincidunt, maurs lorem feugiat augue. id tristique lacus est ac purus
             </p>
-            <a>
+            <a className='font-size-1dot8-vw color-5eccf4 text-shadow-minus-0dot1-vw-9ff2ff-0dot1-vw-11b6ef'>
                 Leer m&aacute;s
             </a>
         </article>
-        <article>
-            <h2>
+        <article className='h-18dot1-vw'>
+            <h2 className='m-00 font-weight-400 font-size-2dot4-vw'>
                 Web Design &amp; Development
             </h2>
-            <p>
+            <p className='line-height-2dot3-vw mt-1dot1-vw mb-1dot3-vw'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a nibg mauris. Mauris interdum, dolor in vulputate tincidunt, maurs lorem feugiat augue. id tristique lacus est ac purus
             </p>
-            <a>
+            <a className='font-size-1dot8-vw color-5eccf4 text-shadow-minus-0dot1-vw-9ff2ff-0dot1-vw-11b6ef'>
                 Leer m&aacute;s
             </a>
         </article>
-        <article>
-            <h2>
+        <article className='h-18dot1-vw'>
+            <h2 className='m-00 font-weight-400 font-size-2dot4-vw'>
                 Usability Testing
             </h2>
-            <p>
+            <p className='line-height-2dot3-vw mt-1dot1-vw mb-1dot3-vw'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a nibg mauris. Mauris interdum, dolor in vulputate tincidunt, maurs lorem feugiat augue. id tristique lacus est ac purus
             </p>
-            <a>
+            <a className='font-size-1dot8-vw color-5eccf4 text-shadow-minus-0dot1-vw-9ff2ff-0dot1-vw-11b6ef'>
                 Leer m&aacute;s
             </a>
         </article>
-        <article>
-            <h2>
+        <article className='h-18dot1-vw'>
+            <h2 className='m-00 font-weight-400 font-size-2dot4-vw'>
                 Interaction Design
             </h2>
-            <p>
+            <p className='line-height-2dot3-vw mt-1dot1-vw mb-1dot3-vw'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a nibg mauris. Mauris interdum, dolor in vulputate tincidunt, maurs lorem feugiat augue. id tristique lacus est ac purus
             </p>
-            <a>
+            <a className='font-size-1dot8-vw color-5eccf4 text-shadow-minus-0dot1-vw-9ff2ff-0dot1-vw-11b6ef'>
                 Leer m&aacute;s
             </a>
         </article>
