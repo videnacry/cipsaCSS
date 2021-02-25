9d815457874f328409a467da9d586907c2b6f657
beron <bleink@outlook.es>
2021-02-25 16:37:46 +0100

    services.js - final project services page html

--- src/proyecto_final/services.js
+++ src/proyecto_final/services.js
@@ -1,7 +1,56 @@
+import {Header} from './header'
+
 export const Services = () => {
     return(
-        <div>
-            Services
-        </div>
+    <>
+    <Header />
+    <div>
+        <h1>Servicios</h1>
+        <article>
+            <h2>
+                User Experience
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a nibg mauris. Mauris interdum, dolor in vulputate tincidunt, maurs lorem feugiat augue. id tristique lacus est ac purus
+            </p>
+            <a>
+                Leer m&aacute;s
+            </a>
+        </article>
+        <article>
+            <h2>
+                Web Design &amp; Development
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a nibg mauris. Mauris interdum, dolor in vulputate tincidunt, maurs lorem feugiat augue. id tristique lacus est ac purus
+            </p>
+            <a>
+                Leer m&aacute;s
+            </a>
+        </article>
+        <article>
+            <h2>
+                Usability Testing
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a nibg mauris. Mauris interdum, dolor in vulputate tincidunt, maurs lorem feugiat augue. id tristique lacus est ac purus
+            </p>
+            <a>
+                Leer m&aacute;s
+            </a>
+        </article>
+        <article>
+            <h2>
+                Interaction Design
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a nibg mauris. Mauris interdum, dolor in vulputate tincidunt, maurs lorem feugiat augue. id tristique lacus est ac purus
+            </p>
+            <a>
+                Leer m&aacute;s
+            </a>
+        </article>
+    </div>
+    </>
     )
 }
