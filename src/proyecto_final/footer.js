d5078a8cd681bff30f94a70c7463acdc76a7d139
beron <bleink@outlook.es>
2021-02-26 11:19:20 +0100

    footer.js - styles.cs - footer component from final project, html & style

---
+++ src/proyecto_final/footer.js
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return(
+    <footer className='border-box h-03-vw mb-10-vw border-00 border-t-00dot3-vw border-solid border-color-e5e5e5 flex-column justify-content-center bg-color-f1f1f2'>
+        <p className='m-00 font-size-1dot1-vw font-weight-500 color-9c89a2 text-align-center'>
+            Copyright © 2011 - Cipsa.Net
+        </p>
+    </footer>
+    )
+}
