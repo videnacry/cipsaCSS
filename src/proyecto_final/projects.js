efd284e1fad207c68158f6d6e19d74a52179a5b7
beron <bleink@outlook.es>
2021-02-26 10:35:13 +0100

    projects.js & style.css - final project projects page styles

--- src/proyecto_final/projects.js
+++ src/proyecto_final/projects.js
@@ -1,7 +1,58 @@
+import {Header} from './header'
+
 export const Projects = () => {
     return(
-        <div>
-            Projects
-        </div>
+    <>
+    <Header/>
+    <div className='border-box pl-4dot9-vw pr-05-vw font-weight-400 font-size-1dot6-vw color-4d5155 text-shadow-minus-0dot1-vw-fcdfaa-0dot1-vw-b5c2d9-0-0-0dot1-vw-6e5571'>
+        <h1 className='m-00 mt-4dot4-vw font-weight-500 font-size-3dot1-vw'>
+            Proyectos
+        </h1>
+        <article className='border-box h-24dot4-vw pt-4dot8-vw border-00 border-b-00dot25-vw border-dotted border-color-e5e5e5'>
+            <div className='w-40-vw float-left'>
+                <h2 className='m-00 mt-2dot2-vw font-weight-400 font-size-03-vw line-height-2dot3-vw'>
+                    Lorem ipsum
+                </h2>
+                <p className='line-height-2dot3-vw mt-1dot5-vw mb-1dot3-vw'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a nibg mauris. Mauris interdum, dolor in vulputate tincidunt, maurs lorem feugiat augue. id tristique lacus est ac purus
+                </p>
+            </div>
+            <div className='border-box h-13dot8-vw w-22dot4-vw float-right border-00dot3-vw border-solid border-color-e5e5e5 font-size-1dot8-vw line-height-1dot2-vw'>akono215x130</div>
+        </article>
+        <article className='border-box h-25dot2-vw pt-5dot5-vw border-00 border-b-00dot25-vw border-dotted border-color-e5e5e5'>
+            <div className='w-40-vw float-left'>
+                <h2 className='m-00 mt-2dot2-vw font-weight-400 font-size-03-vw line-height-2dot3-vw'>
+                    Lorem ipsum
+                </h2>
+                <p className='line-height-2dot3-vw mt-1dot5-vw mb-1dot3-vw'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a nibg mauris. Mauris interdum, dolor in vulputate tincidunt, maurs lorem feugiat augue. id tristique lacus est ac purus
+                </p>
+            </div>
+            <div className='border-box h-13dot8-vw w-22dot4-vw float-right border-00dot3-vw border-solid border-color-e5e5e5 font-size-1dot8-vw line-height-1dot2-vw'>akono215x130</div>
+        </article>
+        <article className='border-box h-25dot2-vw pt-5dot5-vw border-00 border-b-00dot25-vw border-dotted border-color-e5e5e5'>
+            <div className='w-40-vw float-left'>
+                <h2 className='m-00 mt-2dot2-vw font-weight-400 font-size-03-vw line-height-2dot3-vw'>
+                    Lorem ipsum
+                </h2>
+                <p className='line-height-2dot3-vw mt-1dot5-vw mb-1dot3-vw'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a nibg mauris. Mauris interdum, dolor in vulputate tincidunt, maurs lorem feugiat augue. id tristique lacus est ac purus
+                </p>
+            </div>
+            <div className='border-box h-13dot8-vw w-22dot4-vw float-right border-00dot3-vw border-solid border-color-e5e5e5 font-size-1dot8-vw line-height-1dot2-vw'>akono215x130</div>
+        </article>
+        <article className='h-25dot2-vw border-00  pt-5dot5-vw'>
+            <div className='w-40-vw float-left'>
+                <h2 className='m-00 mt-2dot2-vw font-weight-400 font-size-03-vw line-height-2dot3-vw'>
+                    Lorem ipsum
+                </h2>
+                <p className='line-height-2dot3-vw mt-1dot5-vw mb-1dot3-vw'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a nibg mauris. Mauris interdum, dolor in vulputate tincidunt, maurs lorem feugiat augue. id tristique lacus est ac purus
+                </p>
+            </div>
+            <div className='border-box h-13dot8-vw w-22dot4-vw float-right border-00dot3-vw border-solid border-color-e5e5e5 font-size-1dot8-vw line-height-1dot2-vw'>akono215x130</div>
+        </article>
+    </div>
+    </>
     )
 }
